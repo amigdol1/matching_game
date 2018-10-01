6cb34afbcae6844221a8af6a133b966ea151cc2a
Alex Migdol <amigdol@cvent.com>
2018-10-01 15:39:18 -0700

    adds logic to win the game

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,17 +1,51 @@
 "use strict";
 
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+function calculateScore(numberOfMoves) {
+  if (numberOfMoves < 25) {
+    return 3;
+  } else if (numberOfMoves < 40) {
+    return 2;
+  } else {
+    return 1;
+  }
+}
+
+function allCardsMatch(cards) {
+  return cards.every(function(card) {
+    return card.set == cards[0].set;
+  })
+}
+
 var game = {
-  numberOfSets: 8,
+  numberOfSets: 2,
   numberOfCardsPerSet: 2,
   sets: [],
   cards: [],
+  cardsInCurrentMove: [],
+  startTime: 0,
+  numberOfMoves: 0,
   // function called for game setup
   setup: function() {
     // creates the number of sets for the game
     for (var i=0; i < this.numberOfSets; i++ ) {
       var set = {
         id: i,
-        cards: []
+        // cards: [],
+        matched: false
       }
       // creates the cards per set
       for (var n=0; n < this.numberOfCardsPerSet; n++) {
@@ -19,19 +53,40 @@ var game = {
           set: set
         }
         // pushes to a specific set
-        set.cards.push(card);
+        // set.cards.push(card);
         // pushes to the card array to refer to later for shuffling
         this.cards.push(card);
       }
       // pushes sets
       this.sets.push(set);
     }
+    this.cards = shuffle(this.cards);
+    this.startTime = Date.now();
+    this.numberOfMoves = 0;
+    this.cardsInCurrentMove = [];
+  },
+  addCardToMove: function(card) {
+    this.cardsInCurrentMove.push(card);
+    if (this.cardsInCurrentMove.length === this.numberOfCardsPerSet) {
+      if (allCardsMatch(this.cardsInCurrentMove)) {
+        console.log("you found a match");
+        this.cardsInCurrentMove[0].set.matched = true;
+      };
+      this.cardsInCurrentMove = [];
+    };
+    if (this.allSetsMatched()) {
+      console.log("youve won the game!!");
+    }
+  },
+  allSetsMatched: function() {
+    return this.sets.every(function(set) {
+      return set.matched === true;
+    })
   }
 };
 
+
 game.setup();
 
-for (var i=0; i < game.cards.length; i++) {
-  var card = game.cards[i];
-  console.log(card.set.id);
-}
+
+window.game = game;
